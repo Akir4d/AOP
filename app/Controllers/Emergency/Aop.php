76a0196fb588131243b973f78f29ee1aace963aa
Paolo Rampino <liadserv@gmail.com>
2022-12-01 09:18:00 +0100

    Redirect emegency to login if not logged

--- app/Controllers/Emergency/Aop.php
+++ app/Controllers/Emergency/Aop.php
@@ -4,6 +4,23 @@ namespace App\Controllers\Emergency;
 
 class Aop extends BaseController
 {
+
+    private function _construct()
+    {
+        $test = explode(' ', $this->request->getServer("HTTP_AUTHORIZATION"));
+        $uuid = "";
+        if ($test[0] = "Bearer") {
+            $session = \Config\Services::session();
+            if ($session->uuid == null) {
+                $session->uuid = Uuid::uuid4()->toString();
+            }
+            $uuid = $test[1];
+            if ($session->uuid !== $uuid) {
+                redirect()->to(base_url('emergency/aop/account/login'));
+            }
+        }
+    }
+
     public function index()
     {
         return $this->aopRender("emergency", "emergency/aop", ['api' => base_url("emergency")]);
